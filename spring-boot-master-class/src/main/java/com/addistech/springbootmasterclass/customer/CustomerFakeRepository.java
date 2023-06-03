71e5ce23c5847ae55a45f8843929f1e6ea1e3791
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-06-03 18:58:11 -0500

    Java bean validation

--- spring-boot-master-class/src/main/java/com/addistech/springbootmasterclass/customer/CustomerFakeRepository.java
+++ spring-boot-master-class/src/main/java/com/addistech/springbootmasterclass/customer/CustomerFakeRepository.java
@@ -1,24 +1,23 @@
 package com.addistech.springbootmasterclass.customer;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 
 import java.util.List;
+
 @Repository(value = "fake")
-public class CustomerFakeRepository implements  CustomerRepo {
+public class CustomerFakeRepository implements CustomerRepo {
     @Override
-    public List<Customer> getCustomers(){
+    public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(100L, "Arsema Yohannes","Arsema"),
-                new Customer(101L, "Hanna Addis","Addis"));
+                new Customer(100L, "Arsema Yohannes", "Arsema", "email@gmail.com"),
+                new Customer(101L, "Hanna Addis", "Addis", "email@gmail.com"));
     }
 
     @Override
-    public  Customer getCustomer(){
+    public Customer getCustomer() {
         //TODO
-        return new Customer(10L, "New Customer ","new Password");
+        return new Customer(10L, "New Customer ", "new Password", "email@gmail.com");
     }
-
 }
